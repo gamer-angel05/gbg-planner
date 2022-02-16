c23196294ea0aa8d94ff8b7134859300245a3668
gamer-angel05 <45619299+gamer-angel05@users.noreply.github.com>
2022-02-15 18:44:08 -0600

    Clean up

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const zones = new Zones();
 const mapGroups = document.querySelectorAll(".map-group");
 
 zones.setupZonesWithMap(mapGroups);
@@ -11,7 +12,35 @@ $('#permalink').on('hidden.bs.tooltip', function () {
   $("#permalink").attr('data-original-title', "Copy to clipboard")
 })
 
+function updateHashMap() {
+	
+	const mapArray = [];
+	var hash = window.location.hash;
+
+	if (hash) {
+		const hashArray = hash.replace("#", "").split("&");
+		hashArray.forEach(mapZone => {
+			let zone = mapZone.split("=");
+			let data = zone[1].split(",");
+			mapArray.push({"zone": zone[0], "owner": data[0], "buildings": data[1]});
+		})
+		zones.importZonesWithHash(mapArray);
+		window.location.hash = "";
+	}
+}
+
 
 function handleResetClick() {
 	zones.resetMapZones();
 }
+
+function __init__() {
+	/*	Load the map if needed
+	*/
+	$('[data-toggle="tooltip"]').tooltip({trigger : 'hover'});
+
+	updateHashMap();
+	window.onhashchange = updateHashMap;
+}
+
+$(document).ready(__init__);
